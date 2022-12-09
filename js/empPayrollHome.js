ed5efbdbe44550c2664a89b5ff4f47b51c1715e9
adityabhartib <adityabharti.ab@gmail.com>
2022-12-10 00:43:22 +0530

    Added : Display Details Using Templets Literals

--- js/empPayrollHome.js
+++ js/empPayrollHome.js
@@ -17,7 +17,7 @@ const createInnerHtml = () => {
                     <div class='dept-label'>HR</div>
                     <div class='dept-label'>Finance</div>
                 </td>
-                <td>3000000</td>
+                <td>4000000</td>
                 <td>2 June 2022</td>
                 <td>
                     <img id="1" onclick="remove(this)" alt="delete" src="/assets/icons/delete-black-18dp.svg">
